01690b5947bf918d9127c36e5611c111eb023a25
Stanley Lai <stanley.lai@telus.com>
2022-11-22 15:41:56 -0800

    updated

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,18 @@
+
+
+
+
+
+
 const sentence = "hello there from lighthouse labs";
+const newSentence = sentence + "\n";
 let delay = 0;
-for (const char of sentence) {
- 
+for (const char of newSentence) {
   setTimeout(() => {
     process.stdout.write (char);
   }, delay) 
   delay += 50
 }
-console.log("\n");
+
+
 
